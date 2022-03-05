bd79c4745a1b9112614c5ea5df752a09d6eeefb1
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2022-03-05 10:16:17 +0600

    bug fixed in some file

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,28 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('.row');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// notification bar 
+const bell = document.getElementById("bell");
+const notifi = document.getElementById("notifi");
+console.log(bell, notifi)
+
+// bell.addEventListener('click', ()=>{
+//     notifi.style = "margin-right: -300px; transition: 0.5s linear"
+// })
+bell.addEventListener("click", ()=>{
+    notifi.classList.toggle('toogle')
+})
+
+// clear notifications
+const clear = document.getElementById('clear')
+const message = document.getElementById('message')
+
+clear.addEventListener('click', ()=>{
+    message.style = "display: none"
+})
+
+
+
+
+
